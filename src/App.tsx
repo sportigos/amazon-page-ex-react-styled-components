e83e63cff8866e31e12d469ebff581cc83de3dee
JanneLei <sportigo1120@gmail.com>
2021-09-18 23:30:30 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import GlobalStyle from "./styles/global"
+import Navbar from "./components/Navbar"
+import Menubar from "./components/Menubar"
+import PageBody from "./components/PageBody"
+
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <Navbar />
+    <Menubar />
+    <PageBody />
+  </>
+)
+
+export default App;
